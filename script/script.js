514425fd5112c640ad3778ed2d4ea4ede133cc76
Andrew Yurlow <yurlow.andrew@yandex.ru>
2020-09-25 11:17:51 +0300

    feat: заполнение полей формы

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileDecription = document.querySelector('.profile__description');
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
 
-editButton.addEventListener('click', function (){
+function openPopup() {
+  let name = document.querySelector('.popup__input_type_name');
+  let description = document.querySelector('.popup__input_type_description');
   popup.classList.add('popup_opened');
-});
+  name.setAttribute('value', profileName.textContent);
+  description.setAttribute('value', profileDecription.textContent);
+}
+
+editButton.addEventListener('click', openPopup);
 
 closeButton.addEventListener('click', function(){
   popup.classList.remove('popup_opened');
