946f095f99e5360b741fe9b5f848cc5bf4a6ee4b
Andrew Yurlow <yurlow.andrew@yandex.ru>
2020-09-25 09:58:52 +0300

    feat: открытие и закрытие формы

---
+++ script/script.js
@@ -0,0 +1,14 @@
+let closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+
+editButton.addEventListener('click', function (){
+  popup.classList.add('popup_opened');
+});
+
+closeButton.addEventListener('click', function(){
+  popup.classList.remove('popup_opened');
+});
+
+
+
