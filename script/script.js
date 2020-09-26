cde07285d184925b58ff84136c957e4492222726
Andrew Yurlow <yurlow.andrew@yandex.ru>
2020-09-26 08:24:01 +0300

    feature: дописать функциональность для кнопок popup

--- script/script.js
+++ script/script.js
@@ -1,22 +1,32 @@
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileDecription = document.querySelector('.profile__description');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputDescription = document.querySelector('.popup__input_type_description');
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
+let popupSaveButton = document.querySelector('.popup__save-button');
 
 function openPopup() {
-  let name = document.querySelector('.popup__input_type_name');
-  let description = document.querySelector('.popup__input_type_description');
   popup.classList.add('popup_opened');
-  name.setAttribute('value', profileName.textContent);
-  description.setAttribute('value', profileDecription.textContent);
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDecription.textContent;
+}
+function closePopup() {
+  inputName.value = '';
+  inputDescription.value = '';
+  popup.classList.remove('popup_opened');
+}
+function saveProfileSettings(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDecription.textContent = inputDescription.value;
+  popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup);
-
-closeButton.addEventListener('click', function(){
-  popup.classList.remove('popup_opened');
-});
+closeButton.addEventListener('click', closePopup);
+popupSaveButton.addEventListener('click', saveProfileSettings);
 
 
 
