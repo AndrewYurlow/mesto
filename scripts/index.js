0d6d547d87f358852879b429bd527a25ecdc9ee7
Andrew Yurlow <yurlow.andrew@yandex.ru>
2020-10-11 06:53:05 +0300

    feat: добавлять карточку от пользователя

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popupEditForm = document.querySelector('.popup_type_edit-form');
 const popupAddCard = document.querySelector('.popup_type_add-card');
 const formEditProfile = document.querySelector('.popup__container_type_edit-form');
+const formAddCard = document.querySelector('.popup__container_type_add-card');
 const editButton = document.querySelector('.profile__edit-button');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
@@ -11,32 +12,46 @@ const closeEditForm = document.querySelector('.popup__close-button_type_edit-for
 const closeAddCard = document.querySelector('.popup__close-button_type_add-card');
 const inputPlaceTitle = document.querySelector('.popup__input_type_place-title');
 const inputPlaceLink = document.querySelector('.popup__input_type_place-link');
+const cardTemplate = document.querySelector('.card-template').content;
+const cards = document.querySelector('.cards');
 
 const initialCards = [
   {
     name: 'Озеро Байкал',
-    link: '../image/baikal.jpg'
+    link: './image/baikal.jpg'
   },
   {
     name: 'Владивосток',
-    link: '../image/vladivostok.jpg'
+    link: './image/vladivostok.jpg'
   },
   {
     name: 'Смоленск',
-    link: '../image/smolensk.jpg'
+    link: './image/smolensk.jpg'
   },
   {
     name: 'Санкт-Петербург',
-    link: '../image/piter.jpg'
+    link: './image/piter.jpg'
   },
   {
     name: 'Москва',
-    link: '../image/moskva.jpg'
+    link: './image/moskva.jpg'
   },
   {
     name: 'Карелия',
-    link: '../image/karelia.jpg'}];
+    link: './image/karelia.jpg'}];
 
+const prepareCard = (item) => {
+  const card = cardTemplate.cloneNode(true);
+  card.querySelector('.card__image').src = item.link;
+  card.querySelector('.card__title').textContent = item.name;
+  return card;
+}
+const showCards = (array) => {
+  array.forEach((item) => {
+    const card = prepareCard(item);
+    cards.append(card);
+  });
+}
 function togglePopup(popup) {
   popup.classList.toggle('popup_opened');
 }
@@ -45,6 +60,16 @@ function saveProfileSettings(event) {
   profileName.textContent = inputName.value;
   profileDecription.textContent = inputDescription.value;
   togglePopup(popupEditForm);
+}
+function addUserCard(event) {
+  event.preventDefault();
+  const userCard = {
+    name: `${inputPlaceTitle.value}`,
+    link: `${inputPlaceLink.value}`
+  };
+  initialCards.splice(0, 0, userCard);
+  cards.prepend(prepareCard(userCard));
+  togglePopup(popupAddCard);
 }
 editButton.addEventListener('click', () => {
   togglePopup(popupEditForm);
@@ -61,6 +86,8 @@ closeAddCard.addEventListener('click', () => {
   togglePopup(popupAddCard);
 });
 formEditProfile.addEventListener('submit', saveProfileSettings);
+formAddCard.addEventListener('submit', addUserCard);
+showCards(initialCards);
 
 
 
